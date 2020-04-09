90702bafdd0b2cc66906ec446d28101a405d5095
jogendra <imjog24@gmail.com>
2020-04-09 22:59:05 +0530

    V2: Book Store with postgre

--- main.go
+++ main.go
@@ -1,89 +1,38 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	"books-list/controllers"
+	"books-list/driver"
+	"books-list/models"
+
 	"github.com/gorilla/mux"
+	"github.com/subosito/gotenv"
 )
 
-type Book struct {
-	ID     string `json:id`
-	Title  string `json:title`
-	Author string `json:author`
-	Year   string `json:year`
-}
+var books []models.Book
+var db *sql.DB
 
-var books []Book
+func init() {
+	gotenv.Load()
+}
 
 func main() {
+	db = driver.ConnectDB()
+	controller := controllers.Controller{}
+
 	router := mux.NewRouter()
 
-	books = append(books,
-		Book{ID: "1", Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
-		Book{ID: "2", Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
-		Book{ID: "3", Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
-		Book{ID: "4", Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
-		Book{ID: "5", Title: "Golang good parts", Author: "Mr. Good", Year: "2014"})
+	router.HandleFunc("/books", controller.GetBooks(db)).Methods("GET")
+	router.HandleFunc("/books/{id}", controller.GetBook(db)).Methods("GET")
+	router.HandleFunc("/books", controller.AddBook(db)).Methods("POST")
+	router.HandleFunc("/books", controller.UpdateBook(db)).Methods("PUT")
+	router.HandleFunc("/books/{id}", controller.RemoveBook(db)).Methods("DELETE")
 
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books", addBook).Methods("POST")
-	router.HandleFunc("/books", updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
-
+	fmt.Println("Server is running at port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	fmt.Println(params)
-
-	for _, book := range books {
-		if book.ID == params["id"] {
-			json.NewEncoder(w).Encode(&book)
-		}
-	}
-}
-
-func addBook(w http.ResponseWriter, r *http.Request) {
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-
-	books = append(books, book)
-	json.NewEncoder(w).Encode(books)
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-
-	for i, item := range books {
-		if item.ID == book.ID {
-			books[i] = book
-		}
-	}
-
-	json.NewEncoder(w).Encode(books)
-}
-
-func removeBook(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var book Book
-
-	book.ID = params["id"]
-
-	for i, item := range books {
-		if item.ID == book.ID {
-			books = append(books[:i], books[i+1:]...)
-		}
-	}
-
-	json.NewEncoder(w).Encode(books)
 }
